36306fa517cc6f667b6038957626e644daba11d7
Blancaclaire <clara.alonso.jimenez@gmail.com>
2026-02-09 10:30:18 +0100

    Arreglo en lo estilos del boton de registro

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      component: () => import('../views/HomeEnterpriseView.vue'),
     },
     {
       path: '/login',
