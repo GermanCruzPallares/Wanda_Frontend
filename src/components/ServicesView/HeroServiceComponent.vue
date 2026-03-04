c4f5b13a37a3342c66e79622942d9cdedfc8ac8d
Blancaclaire <clara.alonso.jimenez@gmail.com>
2026-03-04 10:12:45 +0100

    Añadida nueva logica para tratar objetivos archivados

--- src/components/ServicesView/HeroServiceComponent.vue
+++ src/components/ServicesView/HeroServiceComponent.vue
@@ -4,24 +4,28 @@
 
 <template>
 
-<section class="hero-service">
-      <div class="hero-service__container">
-        <div class="hero-service__info-card">
-          <h1 class="hero-service__title">¿Qué es Wanda?</h1>
-          <p class="hero-service__description">
-            WANDA es una aplicación web diseñada para ayudarte a gestionar tu
-            economía personal de forma sencilla y visual. Controla tus gastos,
-            planifica tu ahorro y organiza tus finanzas diarias sin
-            complicaciones.
-          </p>
-          <button class="black-button">Empieza</button>
+  <section class="hero-service">
+    <div class="hero-service__container">
+      <div class="hero-service__info-card">
+        <h1 class="hero-service__title">¿Qué es Wanda?</h1>
+        <p class="hero-service__description">
+          WANDA es una aplicación web diseñada para ayudarte a gestionar tu
+          economía personal de forma sencilla y visual. Controla tus gastos,
+          planifica tu ahorro y organiza tus finanzas diarias sin
+          complicaciones.
+        </p>
+        <div class="hero__bt">
+          <button>
+            <RouterLink to="/register"><a href="./signUp.html">Empieza</a></RouterLink>
+          </button>
         </div>
-
-      </div>
-      <div class="hero-service__img">
       </div>
 
-    </section>
+    </div>
+    <div class="hero-service__img">
+    </div>
 
-    
+  </section>
+
+
 </template>
